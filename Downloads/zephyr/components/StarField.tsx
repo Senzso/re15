35712218a06097d82caa44d5a6d14f7a383860b0
Your Name <your-email@example.com>
2025-03-03 22:54:54 +0100

    s

--- Downloads/zephyr/components/StarField.tsx
+++ Downloads/zephyr/components/StarField.tsx
@@ -3,26 +3,24 @@
 import React, { useRef } from "react"
 import { Canvas, useFrame } from "@react-three/fiber"
 import { Points, PointMaterial } from "@react-three/drei"
+import * as random from "maath/random/dist/maath-random.esm"
 import type * as THREE from "three"
-import { random } from "maath"
 
 const StarFieldInner = () => {
   const ref = useRef<THREE.Points>(null!)
 
-  const [positions] = React.useState(() => {
-    const pos = new Float32Array(5000 * 3)
-    random.inSphere(pos, { radius: 1.5 })
-    return pos
-  })
+  const [sphere] = React.useState(() => random.inSphere(new Float32Array(5000), { radius: 1.5 }) as Float32Array)
 
   useFrame((state, delta) => {
-    ref.current.rotation.x -= delta / 10
-    ref.current.rotation.y -= delta / 15
+    if (ref.current) {
+      ref.current.rotation.x -= delta / 10
+      ref.current.rotation.y -= delta / 15
+    }
   })
 
   return (
     <group rotation={[0, 0, Math.PI / 4]}>
-      <Points ref={ref} positions={positions} stride={3}>
+      <Points ref={ref} positions={sphere} stride={3} frustumCulled={false}>
         <PointMaterial transparent color="#ffffff" size={0.003} sizeAttenuation={true} depthWrite={false} />
       </Points>
     </group>

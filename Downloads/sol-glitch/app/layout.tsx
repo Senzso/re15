e9fe5ac90fa5775e3559a4da7a9a0169cb274218
Your Name <your-email@example.com>
2025-03-05 20:18:33 +0100

    4

--- Downloads/sol-glitch/app/layout.tsx
+++ Downloads/sol-glitch/app/layout.tsx
@@ -9,7 +9,7 @@ export const metadata: Metadata = {
   title: "Net Infinite Gains Glitch Automation | Revolutionary Solana Rewards Token",
   description:
     "The revolutionary Solana rewards token that pays holders in SOL when trading volume targets are reached.",
-    generator: 'v0.dev'
+  generator: "v0.dev",
 }
 
 export default function RootLayout({
@@ -19,11 +19,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link
+          rel="icon"
+          href="https://thefader-res.cloudinary.com/private_images/w_760,c_limit,f_auto,q_auto:best/27georgefloyd-articleLarge_swngph/the-rap-report-to-george-floyd-aka-big-floyd-of-the-legendary-screwed-up-click.jpg"
+        />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   )
 }
 
-
-
-import './globals.css'
